47e4727d063b39402ecb8ab785159b7b8529ff51
Tyandrer Boldt <tyandrer.boldt@daten.com.br>
2025-03-04 00:45:04 -0300

    fix

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDXhZZGO7CtrbTUvamgJmmJ_2v3yWYUmfg",
+    authDomain: "menuchat-a215e.firebaseapp.com",
+    databaseURL: "https://menuchat-a215e-default-rtdb.firebaseio.com",
+    projectId: "menuchat-a215e",
+    storageBucket: "menuchat-a215e.firebasestorage.app",
+    messagingSenderId: "1002439982066",
+    appId: "1:1002439982066:web:1a351f64ca5cbdadd8896b",
+    measurementId: "G-RJP6BSXYGD"
+}; 
